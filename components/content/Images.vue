1e5c0e47f55ffedb2fe3af102b546a8b388d862f
ingrid <ingrid7613@gmail.com>
2023-02-18 18:25:52 +0800

    test public image

---
+++ components/content/Images.vue
@@ -0,0 +1,20 @@
+<script setup>
+    const publicURL = process.env.npm_lifecycle_event === 'generate'? '/personal_website_v4': ''
+    const props = defineProps({
+        path: {
+            type: String,
+            default: ''
+        },
+        alt: {
+            type: String,
+            default: ''
+        }
+    })
+</script>
+<template>
+    <img 
+        :src="`${publicURL}${props.path}`"
+        :alt="props.alt"
+        class="max-h-96 mx-auto" 
+    />
+</template>
