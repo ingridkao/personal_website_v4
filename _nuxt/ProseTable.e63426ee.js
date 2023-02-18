af4d78a942ba10393213922245a7eb3e4eabdb9e
ingridkao <ingridkao@users.noreply.github.com>
2023-02-18 05:49:38 +0000

    deploy: f77c96e98cb56a5572d7c6aaea115bd97941e71a

---
+++ _nuxt/ProseTable.e63426ee.js
@@ -0,0 +1,1 @@
+import{q as r,o,s,r as t}from"./entry.5bc028e1.js";const a={};function n(e,c){return o(),s("table",null,[t(e.$slots,"default")])}const f=r(a,[["render",n]]);export{f as default};
---
+++ _nuxt/ProseTr.8642cb41.js
@@ -0,0 +1,1 @@
+import{q as r,o,s,r as t}from"./entry.5bc028e1.js";const n={};function c(e,a){return o(),s("tr",null,[t(e.$slots,"default")])}const f=r(n,[["render",c]]);export{f as default};
